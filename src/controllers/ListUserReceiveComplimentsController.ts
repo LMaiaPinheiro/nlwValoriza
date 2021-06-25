5b03b44825c175e07a8bed390b7977fa6946de87
Lucas Pinheiro <77104894+LMaiaPinheiro@users.noreply.github.com>
2021-06-24 22:27:01 -0300

    Initial commit

---
+++ src/controllers/ListUserReceiveComplimentsController.ts
@@ -0,0 +1,19 @@
+import {Request, Response} from "express"
+import {ListUserReceiveComplimentsService} from "../services/ListUserReceiveComplimentsService"
+
+class ListUserReceiveComplimentsController{
+
+  async handle(request:Request, response:Response){
+
+    const {user_id} = request
+
+    const listUserReceiverComplimentsService = new ListUserReceiveComplimentsService();
+
+    const components = await listUserReceiverComplimentsService.execute(user_id)
+
+    return response.json(components)
+  }
+
+}
+
+export {ListUserReceiveComplimentsController}
